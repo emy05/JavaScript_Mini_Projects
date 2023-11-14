527799d029acb3e91edf21f07a07e649cce5310d
EMY PREMLIN <emypremlin2003@gmail.com>
2023-11-15 00:49:24 +0530

    init commit

---
+++ Animations/index.js
@@ -0,0 +1,10 @@
+var typed = new Typed('#myText' , {
+    strings : ['Javascript Animations', 'Javascript Animations','Javascript Animations'],
+    typeSpeed: 150,
+    backSpeed: 50,
+    loop: true
+})
+AOS.init({
+    duration:3000
+})
+//AOS- Animate On Scroll Library
